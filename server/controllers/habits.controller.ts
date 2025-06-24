73c1479ad113a541016e06211ff420323a309556
Gustavo Coelho <gustavocorvelho@gmail.com>
2025-06-24 18:51:55 -0300

    Add update and delete habit routes with Prisma

--- server/controllers/habits.controller.ts
+++ server/controllers/habits.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
+//GET HABITS
 export async function getHabits(req: Request, res: Response) {
   try {
     const habits = await prisma.habit.findMany();
@@ -13,6 +13,37 @@ export async function getHabits(req: Request, res: Response) {
   }
 }
 
+//DELETE HABITS
+export async function deleteHabits(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const habits = await prisma.habit.delete({
+      where: { id },
+    });
+    res.status(200).json({message: 'Habit deleted successfully'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error deleting habits' });
+  }
+}
+
+//UPDATE HABITS
+export async function editHabits(req: Request, res: Response) {
+  const { title } = req.body;
+  const { id } = req.params;
+  try {
+    const habits = await prisma.habit.update( {
+      where: { id },
+      data: { title }
+    });
+    res.status(200).json({message: 'Habit updated successfully'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error updating habits' });
+  }
+}
+
+//CREATE HABITS
 export async function createHabit(req: Request, res: Response) {
   const { title } = req.body;
 
